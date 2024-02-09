c4f975469ce45cd0e7c53cd5e5f23385d0803173
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2024-02-09 15:36:28 +0300

    🆕 first commit.

---
+++ src/main/java/com/wenthor/batchJShell/Application.java
@@ -0,0 +1,13 @@
+package com.wenthor.batchJShell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
+
+@CommandScan
+@SpringBootApplication
+public class Application{
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
